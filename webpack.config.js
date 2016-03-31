99e539e6278cfe0a2d6b22c2be06d05ddf5147c8
goblinsan <coghlanjames@yahoo.com>
2016-03-31 09:53:31 -0400

    basic working template

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/src/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules!postcss'
+      }
+    ]
+  },
+
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  plugins: [
+    new webpack.BannerPlugin("Copyright LCH Consulting")
+  ],
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.tmpl.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devServer: {
+    contentBase: "./build",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  } 
+}
